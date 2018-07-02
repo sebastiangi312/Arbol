a951101ecb08cadb0f09d46592e381ffe9ac39fd
sebastiangi312 <40746232+sebastiangi312@users.noreply.github.com>
2018-07-01 21:42:46 -0500

    Update Arbol.c

---
+++ Arbol.c
@@ -0,0 +1,309 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+
+typedef struct nodo{				//Estructura de tipo nodo en la cual se almacenan el dato y las referencias a los hijos
+	char dato;
+	struct nodo *ptrIzq;
+	struct nodo *ptrDer;
+}Nodo;
+
+typedef struct pila{
+	int size;				//Pila en la cual se almacenaran las referencias a los padres
+	struct nodo *ptrAnterior;
+	struct pila *ptrSiguiente;
+}Pila;
+
+void AgregarPila(Pila **ptrInicio,Nodo *ptrNodo);
+Nodo* EliminarPila(Pila **ptrInicio);
+void AgregarArbol(Nodo **ptrRaiz,Nodo **ptrActual,Pila **ptrPila,char dato);
+void EncontrarSiguiente(Nodo **ptrActual,Pila **ptrPila);
+void ResolverArbol(Nodo *ptrActual,Pila *ptrPila);
+void ImprimirVertical(Nodo *ptrRaiz);
+void Reubicar(Nodo **ptrActual,Pila **ptrPila,int *NivelMax,int *nivel,int *NivelAux);
+int Niveles(Nodo *ptrRaiz);
+void OperadorNum(char dato);
+
+int main() {
+	Pila *ptrPila=NULL;
+	Nodo *ptrRaiz=NULL;
+	Nodo *ptrActual=NULL;
+	int Eleccion;
+	char dato;
+	printf("Digite 1 para agregar un dato u operador,2 para Mostrar el arbol y 3 para resolver el arbol y 4 para acabar la ejecucion\n");
+	scanf("%d",&Eleccion);
+	while(Eleccion!=4){
+		switch(Eleccion){
+			case 1:
+				printf("Ingrese el dato\n");
+				scanf("%s",&dato);
+				AgregarArbol(&ptrRaiz,&ptrActual,&ptrPila,dato);
+				break;
+			case 2:
+				ImprimirVertical(ptrRaiz);
+				break;
+			case 3:
+				Pila *ptrPilaAux=ptrPila;
+				Nodo *ptrActualAux=ptrActual;
+				ResolverArbol(ptrActualAux,ptrPilaAux);
+				break;
+		}
+		printf("Digite 1 para agregar un dato y 2 para Mostrar el arbol\n");
+		scanf("%d",&Eleccion);
+	}
+	return 0;
+}
+
+void AgregarArbol(Nodo **ptrRaiz,Nodo **ptrActual,Pila **ptrPila,char dato){  //Funcion que unicamente se encarga de crear la raiz,y llenar los hijos.
+	int i=0;
+	if(*ptrRaiz==NULL){							
+		*ptrRaiz=(Nodo*)malloc(sizeof(Nodo));			//Si la raiz no existe este la crea y hace que el puntero a la raiz y el
+		(*ptrRaiz)->dato=dato;					// puntero Actual(en el que se agregara el siguiente nodo) apunten a ella.
+		(*ptrRaiz)->ptrDer=NULL;
+		(*ptrRaiz)->ptrIzq=NULL;
+		*ptrActual=*ptrRaiz;
+	}else{	
+		if((*ptrActual)->ptrIzq==NULL){			//En caso de que el puntero actual no tenga hijo izquierdo este lo crea y llama la funcion ordenar.
+			Nodo *ptrHijo= (Nodo*)malloc(sizeof(Nodo));
+			ptrHijo->ptrDer=NULL;
+			ptrHijo->ptrIzq=NULL;
+			ptrHijo->dato=dato;
+			(*ptrActual)->ptrIzq=ptrHijo;
+			AgregarPila(ptrPila,*ptrActual);
+			EliminarPila(ptrPila);
+		}else{
+			Nodo *ptrHijo=(Nodo*)malloc(sizeof(Nodo)); //En caso de que el puntero actual tenga hijo izquierdo este creara el hijo derecho y ejecutara la funcion para reubicar el puntero actual.
+			ptrHijo->dato=dato;				
+			ptrHijo->ptrDer=NULL;
+			ptrHijo->ptrIzq=NULL;
+			(*ptrActual)->ptrDer=ptrHijo;
+			AgregarPila(ptrPila,*ptrActual);
+			EliminarPila(ptrPila);
+			EncontrarSiguiente(ptrActual,ptrPila);
+		}
+	}
+}
+void EncontrarSiguiente(Nodo **ptrActual,Pila **ptrPila){	//Funcion que se encarga de reubicar el puntero actual al nodo en el cual debe ingresarse el siguiente dato.
+
+	Nodo *ptrPadre=EliminarPila(ptrPila);		//Esta funcion crea un puntero llamado puntero Padre y en el ingresara el la referencia del padre del puntero actual atravez de la pila,Aqui se pueden dar 2 casos. 
+	if(ptrPadre==NULL){										
+		while((*ptrActual)->ptrIzq!=NULL){			//Primer caso: Que el puntero padre apunte a NULL,lo cual quiere decir que el puntero actual es la raiz y esto solo pasara cuando todos los niveles esten llenos,
+			AgregarPila(ptrPila,*ptrActual);		//Por lo tanto se ubicara en el Nodo Hoja que se encuentre mas a la izquierda(Guardando en la pila todas las posiciones por las que pasa) y desde aqui procedera a crear otro nivel cuando se le ingrese el siguiente dato.
+			*ptrActual=(*ptrActual)->ptrIzq;	
+		}
+	}else{											//Segundo caso: Que este exista y de aqui se desembocan 2 casos:
+	
+		if(ptrPadre->ptrDer!=*ptrActual){			//Primer caso: Que el puntero Actual sea el hijo izquierdo de su padre y en este caso el puntero actual pasara a apuntar a su hermano(hijo derecho de su padre)
+			AgregarPila(ptrPila,ptrPadre);			//y de aqui se ubicara en el Nodo hoja que se encuentre mas a la izquierda de este (Guardando en la pila todas las posiciones por las que pasa).
+			*ptrActual=ptrPadre->ptrDer;
+			while((*ptrActual)->ptrIzq!=NULL){
+				AgregarPila(ptrPila,*ptrActual);
+				*ptrActual=(*ptrActual)->ptrIzq;	
+			}
+			}else{
+			*ptrActual=ptrPadre;					//Segundo Caso:Que el puntero Actual sea el hijo derecho de su padre, en este caso el puntero actual apuntara a su padre y llamara recursivamente la funcion.
+			EncontrarSiguiente(ptrActual,ptrPila);
+		}
+	}
+}
+
+void AgregarPila(Pila **ptrInicio,Nodo *ptrNodo){			//Pila en la cual se guardaran las direcciones de los Nodos padres.
+
+	if(*ptrInicio==NULL){									//Si la Pila esta vacia este le agrega el primer dato, almacena la direccion del Nodo Padre y actualiza el puntero a la pila.
+		Pila *ptrAux=(Pila*)malloc(sizeof(Pila));
+		ptrAux->ptrAnterior=ptrNodo;
+		ptrAux->ptrSiguiente=NULL;
+		*ptrInicio=ptrAux;
+	}else{													//En caso de que esta no esta vacia agrega un dato,hace que este guarde la referencia al que hay al lado de el y actualiza el puntero a la pila.
+		Pila *ptrAux=(Pila*)malloc(sizeof(Pila));
+		ptrAux->ptrAnterior=ptrNodo;
+		ptrAux->ptrSiguiente=*ptrInicio;
+		*ptrInicio=ptrAux;
+	}
+}
+Nodo* EliminarPila(Pila **ptrInicio){				//Si la Pila tiene datos devuelve el ultimo y en caso de estar vacia devuelve NULL.
+	if(*ptrInicio!=NULL){
+		Nodo *ptrAux=(*ptrInicio)->ptrAnterior;
+		*ptrInicio=(*ptrInicio)->ptrSiguiente;
+		return ptrAux;
+	}else{
+		return NULL;
+	}
+}
+
+void ImprimirVertical(Nodo *ptrRaiz){	//Funcion encargada de imprimir de forma vertical el arbol
+	Pila *ptrPila=NULL;
+	Nodo *ptrActual=ptrRaiz;
+	int EspInicio,i,EspMedio,nivelActual,nivelAux;
+	int NivelMax=Niveles(ptrRaiz);		//Mediante la funcion Niveles se calcula la cantidad de niveles que tiene el arbol.
+	nivelActual=0;
+	nivelAux=0;
+	int *ptrNivelAux=&nivelAux;
+	int *ptrNivelMax=&NivelMax;
+	int *ptrNivelActual=&nivelActual;
+	if(ptrRaiz==NULL){					//Si el puntero a la Raiz no apunta a nada este imprimira que el arbol esta vacio.
+		printf("El arbol esta vacio\n");
+	}else{
+		printf("\n");
+		EspInicio=pow(2,NivelMax)-1;		//Mediante la ecuacion 2^n-1(siendo n la cantidad de niveles(Iniciales) del arbol) se calcula la primera cantidad de espacios.
+		for(i=0;i<EspInicio;i++){
+			printf(" ");
+		}
+		OperadorNum(ptrActual->dato); 	//se imprimira el dato y pasara a la siguiente linea y restara en 1 la cantidad de niveles.
+		printf("\n");
+		NivelMax--;
+		while(ptrActual->ptrIzq!=NULL){		//Mientras el puntero Actual tenga un hijo izquierdo, este verificara si tiene hijo derecho, en caso de no tener
+			if(ptrActual->ptrDer==NULL){	//se da por sentado que este es el ultimo dato del arbol. 
+				OperadorNum(ptrActual->ptrIzq->dato);
+				printf("\n");
+				break;
+			}else{
+				if(nivelActual==0){
+					EspInicio=pow(2,NivelMax)-1;
+					for(i=0;i<EspInicio;i++){
+						printf(" ");
+					}
+				}
+				OperadorNum(ptrActual->ptrIzq->dato);
+				EspMedio=pow(2,NivelMax+1)-1;
+				for(i=0;i<EspMedio;i++){
+					printf(" ");
+				}
+				OperadorNum(ptrActual->ptrDer->dato);
+				for(i=0;i<EspMedio;i++){
+					printf(" ");
+				}
+				Reubicar(&ptrActual,&ptrPila,ptrNivelMax,ptrNivelActual,ptrNivelAux);
+			}
+		}
+		printf("\n");
+	} 
+	
+}
+void Reubicar(Nodo **ptrActual,Pila **ptrPila,int *NivelMax,int *nivel,int *NivelAux){
+	Nodo *ptrPadre=EliminarPila(ptrPila);	
+	int NE,i;
+	if(ptrPadre==NULL){
+		*NivelMax=*NivelMax-1;
+		*nivel=*nivel+1;
+		for(i=0;i<*nivel;i++){
+			AgregarPila(ptrPila,*ptrActual);
+			*ptrActual=(*ptrActual)->ptrIzq;
+			*NivelAux=*NivelAux+1;
+		}
+		printf("\n");
+		NE=pow(2,*NivelMax)-1;
+		for(i=0;i<NE;i++){
+			printf(" ");
+		}
+	}else{
+		if(ptrPadre->ptrDer!=*ptrActual){			
+			AgregarPila(ptrPila,ptrPadre);			
+			*ptrActual=ptrPadre->ptrDer;
+			
+			for(i=0;i<*nivel-*NivelAux;i++){
+				AgregarPila(ptrPila,*ptrActual);
+				*ptrActual=(*ptrActual)->ptrIzq;
+			}
+			*NivelAux=*nivel;
+		}else{
+			*ptrActual=ptrPadre;					
+			*NivelAux=*NivelAux-1;
+			Reubicar(ptrActual,ptrPila,NivelMax,nivel,NivelAux);
+		}
+	}
+}
+int Niveles(Nodo *ptrRaiz){		//funcion que calcula y devuelve el numero de niveles.
+	Nodo *ptrAux=ptrRaiz;
+	int Nivel=0;
+	if(ptrAux!=NULL){			//Mientras el puntero en cuestion tenga hijo izquiero el puntero apuntara a este y aumentara en 1 a la variable nivel.
+		while(ptrAux->ptrIzq!=NULL ){
+			ptrAux=ptrAux->ptrIzq;
+			Nivel++;
+		}
+	}
+	return Nivel;
+}
+
+
+void ResolverArbol(Nodo *ptrActual,Pila *ptrPila){
+	if(ptrActual->ptrIzq!=NULL){
+		printf("\nArbol Invalido\n");
+	}else{
+		Nodo *ptrPadre=EliminarPila(&ptrPila);
+		if(ptrPadre->ptrIzq==ptrActual){
+			int aux=0;
+			int a=(int)(ptrPadre->ptrIzq)->dato-48;
+			int b=(int)(ptrPadre->ptrDer)->dato-48;
+			switch(ptrPadre->dato){
+				case '+':
+					aux=a + b;
+					ptrPadre->dato=aux;
+					printf("%d\n",aux);
+					break;
+				case '-':
+					aux=a - b;
+					printf("%d\n",aux);
+					break;
+				case '*':
+					aux=a * b;
+					printf("%d\n",aux);
+					break;
+				case '/':
+					aux=a / b;
+					printf("%f\n",aux);
+					break;
+				default:
+					printf("Error\n");
+					break;
+			}
+		}
+	}	
+}
+
+
+void OperadorNum(char dato){
+	switch(dato){
+		case '+':
+			printf("+");
+			break;
+		case '-':
+			printf("-");
+			break;
+		case '*':
+			printf("*");
+			break;
+		case '/':
+			printf("/");
+			break;
+		default:
+			printf("%d",dato-48);
+			break;
+	}
+}
+
+void EncontrarAnterior(Nodo **ptrActual,Pila **ptrPila){	//Funcion que se encarga de reubicar el puntero actual al nodo en el cual debe ingresarse el siguiente dato.
+
+	Nodo *ptrPadre=EliminarPila(ptrPila);		//Esta funcion crea un puntero llamado puntero Padre y en el ingresara el la referencia del padre del puntero actual atravez de la pila,Aqui se pueden dar 2 casos. 
+	if(ptrPadre==NULL){			
+									
+		while((*ptrActual)->ptrIzq!=NULL){			//Primer caso: Que el puntero padre apunte a NULL,lo cual quiere decir que el puntero actual es la raiz y esto solo pasara cuando todos los niveles esten llenos,
+			AgregarPila(ptrPila,*ptrActual);		//Por lo tanto se ubicara en el Nodo Hoja que se encuentre mas a la izquierda(Guardando en la pila todas las posiciones por las que pasa) y desde aqui procedera a crear otro nivel cuando se le ingrese el siguiente dato.
+			*ptrActual=(*ptrActual)->ptrIzq;	
+		}
+	}else{											//Segundo caso: Que este exista y de aqui se desembocan 2 casos:
+	
+		if(ptrPadre->ptrDer!=*ptrActual){			//Primer caso: Que el puntero Actual sea el hijo izquierdo de su padre y en este caso el puntero actual pasara a apuntar a su hermano(hijo derecho de su padre)
+			AgregarPila(ptrPila,ptrPadre);			//y de aqui se ubicara en el Nodo hoja que se encuentre mas a la izquierda de este (Guardando en la pila todas las posiciones por las que pasa).
+			*ptrActual=ptrPadre->ptrDer;
+			while((*ptrActual)->ptrIzq!=NULL){
+				AgregarPila(ptrPila,*ptrActual);
+				*ptrActual=(*ptrActual)->ptrIzq;	
+			}
+			}else{
+			*ptrActual=ptrPadre;					//Segundo Caso:Que el puntero Actual sea el hijo derecho de su padre, en este caso el puntero actual apuntara a su padre y llamara recursivamente la funcion.
+			EncontrarSiguiente(ptrActual,ptrPila);
+		}
+	}
+}
